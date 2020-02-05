d3ca991d28c63589aaee8bd4cef28ebc9b2745f7
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-02-05 13:49:18 -0700

    Add files via upload

---
+++ CommandScripts/Invoker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invoker
+{
+    private command m_Command;
+    public void SetCommand(command command)
+    {
+        m_Command = command;
+    }
+    public void ExecuteCommand()
+    {
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    Debug.Log("Left");
+        //}
+        //else if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    Debug.Log("Right");
+        //}
+
+        CommandLog.commands.Enqueue(m_Command);
+        m_Command.execute();
+    }
+}
+
---
+++ CommandScripts/TurnLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLeft : command
+{
+    Rigidbody slimShady;
+    float standUp;
+
+    public TurnLeft (Rigidbody player, float force)
+    {
+        slimShady = player;
+        standUp = force;
+    }
+    public override void execute()
+    {
+        slimShady.AddForce(-standUp * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+}
